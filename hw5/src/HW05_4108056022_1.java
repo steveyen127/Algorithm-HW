72ee27401a2391c5bd26b6f3ed22dbc9fe136728
steveyen127 <54370504+steveyen127@users.noreply.github.com>
2021-04-02 00:10:26 +0800

    2021/4/2 hw5_1

--- hw5/src/HW05_4108056022_1.java
+++ hw5/src/HW05_4108056022_1.java
@@ -2,10 +2,37 @@ public class HW05_4108056022_1 extends LLK{
     public static void main(String[] args) {
         int[][] array = {{1,1},
                          {2,2},
-                         {3,6}};
+                         {3,3}};
+        HW05_4108056022_1 test = new HW05_4108056022_1();
+        System.out.println(test.checkLLK(array));
     }
 
-    
+    void quickSort(double A[], int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        double key = A[left], temp;
+        int i = left, j = right;
+        while (i != j) {
+            while (A[j] > key && i < j) {
+                j--;
+            }
+            while (A[i] <= key && i < j) {
+                i++;
+            }
+            if (i < j) {
+                temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+        temp = A[left];
+        A[left] = A[i];
+        A[i] = temp;
+        quickSort(A, left, i - 1);
+        quickSort(A, i + 1, right);
+
+    }
     public double[]angles(int[][] array,int index){
         double[]angles = new double[array.length];
         for (int i=0;i<array.length; i++){
@@ -15,13 +42,19 @@ public class HW05_4108056022_1 extends LLK{
         return angles;
     }
     public boolean checker(double[]angles){
-
+        quickSort(angles, 0, angles.length-1);
+        for (int i = 0; i < angles.length-1; i++){
+            if(angles[i] == angles[i+1]) return true;
+        }
+        return false;
     }
     @Override
     public boolean checkLLK(int[][] array) {
         double[]angles;
+        boolean flag;
         for(int i = 0; i < array.length; i++){
-            angles = angles(array,i);
+            flag = checker(angles(array,i));
+            if(flag== true) return true;
         }
         return false;
     }
