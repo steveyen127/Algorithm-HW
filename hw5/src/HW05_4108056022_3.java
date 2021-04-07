e8071dec18b214a588ba0fce1b2a8fbfe81c3f62
steveyen127 <54370504+steveyen127@users.noreply.github.com>
2021-04-07 14:26:21 +0800

    (0,0) slope test

--- hw5/src/HW05_4108056022_3.java
+++ hw5/src/HW05_4108056022_3.java
@@ -11,8 +11,8 @@ public class HW05_4108056022_3 extends LLK{
         }
     }
     public boolean duplicate(DataItem[] hashArray, double slope, int hashIndex){
-        for(DataItem curr = hashArray[hashIndex]; curr!= null; curr = curr.nextItem){
-            if(slope == curr.slope) return true;
+        for(DataItem curr = hashArray[hashIndex]; curr!= null && curr.nextItem!=null; curr = curr.nextItem){
+            if(slope == curr.slope && slope == curr.nextItem.slope) return true;
         }
         return false;
     }
@@ -32,40 +32,17 @@ public class HW05_4108056022_3 extends LLK{
         Thread[] t = new Thread[10];
         int size = (int)(array.length*1.7);
         found = false;
-        for(int threadIndex = 0; threadIndex < 10; threadIndex++){
-            final int index = threadIndex;
-            t[threadIndex] = new Thread(()-> {
-                    DataItem[] hashArray=new DataItem[size];
-                    double slope;
-                    for(int i = index; i < array.length; i+=10){
-                        for(int j = i+1; j < array.length;j++){
-                            slope = (double)(array[i][0] - array[j][0]) / (double)(array[i][1] - array[j][1]);
-                            if(insert(hashArray,slope, size)) {
-                                found = true;
-                                break;
-                            }
-                        }
-                        if(found)break;
-                        hashArray = new DataItem[size];
-                    }
-            });
-            t[threadIndex].start();
-        }
-        for(int threadIndex = 0; threadIndex < 10; threadIndex++){
-            try{
-                t[threadIndex].join();
-            }
-            catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-
+                DataItem[] hashArray=new DataItem[size];
+                double slope;
+                for(int i = array.length-1; i > -1; i--) {
+                    slope = (double) (array[i][0]) / (double) (array[i][1]);
+                    if (insert(hashArray, slope, size))return true;
+                }
 
         return found;
     }
     public static void main(String[] args) {
-        int[][] array = {{48471285,46187890},{29017325,54336429},{1111,1111},{2222,2222},{39071816,-13623959},{-68518169,15335968},{5555,5555}};
+        int[][] array = {{48471285,46187890},{29017325,54336429},{1121,1111},{2222,2222},{39071816,-13623959},{-68518169,15335968},{5555,5555}};
         HW05_4108056022_3 test = new HW05_4108056022_3();
         System.out.println(test.checkLLK(array));
     }
