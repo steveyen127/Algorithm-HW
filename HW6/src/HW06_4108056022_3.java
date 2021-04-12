e03c8a6b982fe820ef66c18214155438cc46c85b
steveyen127 <54370504+steveyen127@users.noreply.github.com>
2021-04-12 10:07:39 +0800

    HW06 Added thread _02:5thread; _03:10thread; _04:8thread; _05:15thread;

--- HW6/src/HW06_4108056022_3.java
+++ HW6/src/HW06_4108056022_3.java
@@ -1,5 +1,11 @@
 public class HW06_4108056022_3 extends Dessert_Desert{
-
+    public static void main(String[] args){
+        int [][] A = {{1,1,1,1,1,1,1},{1,3,5,7,9},{1,2,3},{5,4,3,2,1},{2,1,3,2},{6,1,5,8,3,7,9,8,10},{6,8,1,7,10,9,12,11},{2,2,2,1,1,1}};
+        HW06_4108056022_3 t = new HW06_4108056022_3();
+        int[] Ans;
+        Ans=t.maxBlocks(A);
+        for (int an : Ans) System.out.println(an);
+    }
 
     public int solution(int[] arr){
         int n = arr.length;
@@ -11,11 +17,13 @@ public class HW06_4108056022_3 extends Dessert_Desert{
         int m = n-1;
         for(int i = 1; i < n; i++){
             maxOfLeft[i] = Math.max(maxOfLeft[i-1], arr[i]);
-            minOfRight[m-i] = Math.min(minOfRight[n-i], arr[m-i]);
+        }
+        for(int i = n-2; i >= 0; i--){
+            minOfRight[i] = Math.min(minOfRight[i+1],arr[i]);
         }
         int ans=0;
-        for (int i=0;i<n;i++){
-            if(maxOfLeft[i] <= minOfRight[i]) ++ans;
+        for (int i=0;i<n-1;i++){
+            if(maxOfLeft[i] <= minOfRight[i+1]) ans++;
         }
         return ans+1;
     }
